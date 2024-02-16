4a99baa50ffaaa898679c430581b98f986dacd8a
KennyD4Christ <kennyd4christ@gmail.com>
2024-02-16 12:23:13 +0100

    Restored 0-bubble_sort.c

--- sort.h
+++ sort.h
@@ -54,7 +54,6 @@ void swap_ints(int *a, int *b);
 int hoare_partition(int *array, size_t size, int left, int right);
 void hoare_sort(int *array, size_t size, int left, int right);
 void quick_sort_hoare(int *array, size_t size);
-void swap_ints(int *a, int *b);
 
 /**
  *  * enum bool - Enumeration of Boolean values.
